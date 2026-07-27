65200c3fafe3116d5e9476f5e2f4bd7d9d47f067
Diptaksho Palit <34395330+CyberLemonade@users.noreply.github.com>
2026-07-27 11:48:36 -0400

    Update theme.js

--- css/theme.js
+++ css/theme.js
@@ -1,18 +1,6 @@
 const root = document.documentElement;
-const button = document.getElementById("theme-toggle");
 
-button.onclick = () => {
-
-    if (root.dataset.theme === "dark") {
-
-        root.dataset.theme = "light";
-        button.textContent = "☽";
-
-    } else {
-
-        root.dataset.theme = "dark";
-        button.textContent = "☀︎";
-
-    }
-
+document.getElementById("theme-toggle").onclick = () => {
+    root.dataset.theme =
+        root.dataset.theme === "dark" ? "light" : "dark";
 };
