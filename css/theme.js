360f1f52a48b346bd1c0615d3d502d2b6b697020
Diptaksho Palit <34395330+CyberLemonade@users.noreply.github.com>
2026-07-27 12:07:10 -0400

    Update theme.js

--- css/theme.js
+++ css/theme.js
@@ -1,6 +1,7 @@
 const root = document.documentElement;
 
+root.dataset.theme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
 document.getElementById("theme-toggle").onclick = () => {
-    root.dataset.theme =
-        root.dataset.theme === "dark" ? "light" : "dark";
+    root.dataset.theme = root.dataset.theme === "dark" ? "light" : "dark";
 };
